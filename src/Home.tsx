3c081a232c67ba51f5f36b01df8fbc6666617dc9
Elad Heller <elad.heller@applitools.com>
2023-06-15 18:12:58 +0300

    responsive

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,6 +8,7 @@ import {
   Legend,
   ReferenceLine,
   Brush,
+  ResponsiveContainer,
 } from 'recharts';
 import warData from './data.json';
 import { HistoryRecordWithAreas } from './types.d';
@@ -33,14 +34,15 @@ interface ChartLabelProps {
   text: string;
   offset: number;
   top: number;
+  left?: number;
 }
 
 function ChartLabel(props: ChartLabelProps) {
   const {
-    viewBox: { x }, text, offset, top,
+    viewBox: { x }, text, offset, top, left = 0,
   } = props;
   return (
-    <text x={x} y={top} dx={-20} offset={offset} fill="green" fontSize={12} textAnchor="middle">
+    <text x={x + left} y={top} dx={-20} offset={offset} fill="green" fontSize={12} textAnchor="middle">
       {text}
     </text>
   );
@@ -48,33 +50,31 @@ function ChartLabel(props: ChartLabelProps) {
 
 export default function App() {
   return <div className="App">
-
-    <LineChart
-      width={1500}
-      height={800}
-      data={distinctData}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <ReferenceLine x="05/04/2022" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="סיום נסיגת רוסיה מהצפון"/>} />
-      <ReferenceLine x="06/09/2022" stroke="green" label={(props) => <ChartLabel {...props} top={120} text="מתקפת חרקוב"/>} />
-      <ReferenceLine x="01/10/2022" stroke="green" label={(props) => <ChartLabel {...props} top={190} text="שחרור צפון חרסון"/>} />
-      <ReferenceLine x="09/11/2022" stroke="green" label={(props) => <ChartLabel {...props} top={220} text="נסיגת רוסיה מחרסון"/>} />
-      <ReferenceLine x="06/06/2023" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="פיצוץ סכר נובה קחובקה"/>} />
-      <Line type="monotone" dataKey="לא ידוע" stroke="#00c02b" />
-      <Line type="monotone" dataKey="משוחרר" stroke="blue" />
-      <Line type="monotone" dataKey="כבוש" stroke="red" />
-      <Brush dataKey="name" height={30} stroke="#8884d8" />
-    </LineChart>
-  )
+    <ResponsiveContainer width="100%" height={800}>
+      <LineChart
+        data={distinctData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <ReferenceLine x="05/04/2022" stroke="green" label={(props) => <ChartLabel {...props} top={240} left={20} text="סיום נסיגת רוסיה מהצפון"/>} />
+        <ReferenceLine x="06/09/2022" stroke="green" label={(props) => <ChartLabel {...props} top={120} text="מתקפת חרקוב"/>} />
+        <ReferenceLine x="01/10/2022" stroke="green" label={(props) => <ChartLabel {...props} top={190} text="שחרור צפון חרסון"/>} />
+        <ReferenceLine x="09/11/2022" stroke="green" label={(props) => <ChartLabel {...props} top={220} text="נסיגת רוסיה מחרסון"/>} />
+        <ReferenceLine x="06/06/2023" stroke="green" label={(props) => <ChartLabel {...props} top={240} left={-20} text="פיצוץ סכר נובה קחובקה"/>} />
+        <Line type="monotone" dataKey="לא ידוע" stroke="#00c02b" />
+        <Line type="monotone" dataKey="משוחרר" stroke="blue" />
+        <Line type="monotone" dataKey="כבוש" stroke="red" />
+        <Brush dataKey="name" height={30} stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
   </div>;
 }
