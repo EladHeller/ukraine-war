d17246a9761176af2d172fb3f213d609f67afea9
Elad Heller <elad.heller@applitools.com>
2023-06-15 17:33:13 +0300

    add time range

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,19 +7,21 @@ import {
   Tooltip,
   Legend,
   ReferenceLine,
+  Brush,
 } from 'recharts';
 import warData from './data.json';
 import { HistoryRecordWithAreas } from './types.d';
 
 import './Home.scss';
 
-const data = warData.map((war: HistoryRecordWithAreas) => ({
-  name: new Date(war.createdAt).toLocaleDateString(),
-  משוחרר: war.liberated.toFixed(2),
-  כבוש: war.occupied.toFixed(2),
-  'לא ידוע': war.unspecified.toFixed(2),
-}));
-const distinct = data.reverse()
+const distinctData = warData
+  .map((war: HistoryRecordWithAreas) => ({
+    name: new Date(war.createdAt).toLocaleDateString('en-GB'),
+    משוחרר: war.liberated.toFixed(2),
+    כבוש: war.occupied.toFixed(2),
+    'לא ידוע': war.unspecified.toFixed(2),
+  }))
+  .reverse()
   .filter((v, i, a) => a.findIndex((t) => (t.name === v.name)) === i)
   .reverse();
 
@@ -45,11 +47,12 @@ function ChartLabel(props: ChartLabelProps) {
 }
 
 export default function App() {
-  return (
+  return <div className="App">
+
     <LineChart
       width={1500}
       height={800}
-      data={distinct}
+      data={distinctData}
       margin={{
         top: 5,
         right: 30,
@@ -62,14 +65,16 @@ export default function App() {
       <YAxis />
       <Tooltip />
       <Legend />
-      <ReferenceLine x="4/5/2022" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="סיום נסיגת רוסיה מהצפון"/>} />
-      <ReferenceLine x="9/6/2022" stroke="green" label={(props) => <ChartLabel {...props} top={120} text="מתקפת חרקוב"/>} />
-      <ReferenceLine x="10/1/2022" stroke="green" label={(props) => <ChartLabel {...props} top={190} text="שחרור צפון חרסון"/>} />
-      <ReferenceLine x="11/9/2022" stroke="green" label={(props) => <ChartLabel {...props} top={220} text="נסיגת רוסיה מחרסון"/>} />
-      <ReferenceLine x="6/6/2023" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="פיצוץ סכר נובה קחובקה"/>} />
+      <ReferenceLine x="05/04/2022" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="סיום נסיגת רוסיה מהצפון"/>} />
+      <ReferenceLine x="06/09/2022" stroke="green" label={(props) => <ChartLabel {...props} top={120} text="מתקפת חרקוב"/>} />
+      <ReferenceLine x="01/10/2022" stroke="green" label={(props) => <ChartLabel {...props} top={190} text="שחרור צפון חרסון"/>} />
+      <ReferenceLine x="09/11/2022" stroke="green" label={(props) => <ChartLabel {...props} top={220} text="נסיגת רוסיה מחרסון"/>} />
+      <ReferenceLine x="06/06/2023" stroke="green" label={(props) => <ChartLabel {...props} top={240} text="פיצוץ סכר נובה קחובקה"/>} />
       <Line type="monotone" dataKey="לא ידוע" stroke="#00c02b" />
       <Line type="monotone" dataKey="משוחרר" stroke="blue" />
       <Line type="monotone" dataKey="כבוש" stroke="red" />
+      <Brush dataKey="name" height={30} stroke="#8884d8" />
     </LineChart>
-  );
+  )
+  </div>;
 }
