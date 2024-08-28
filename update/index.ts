dd999c2a08e7d6cc1d34f78ea0162dca96b558ad
Elad Heller <eladheller@gmail.com>
2024-08-29 02:07:31 +0300

    add occupied in russia.

--- update/index.ts
+++ update/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax, no-console */
 import fs from 'fs/promises';
-import { HistoryRecord, HistoryRecordWithAreas, WarArea } from '../src/types.d';
+import {
+  Areas, HistoryRecord, HistoryRecordWithAreas, WarArea,
+} from '../src/types.d';
 
 async function getHistoryRecords() {
   const historyRecords: HistoryRecord[] = await fetch('https://deepstatemap.live/api/history/public').then((res) => res.json());
@@ -8,15 +10,26 @@ async function getHistoryRecords() {
 }
 async function getAreasStatusById(dateId: number) {
   const areas: WarArea[] = await fetch(`https://deepstatemap.live/api/history/${dateId}/areas`).then((res) => res.json());
-
+  console.log({ areas, dateId });
   const occupied = areas.find((area) => area.type === 'occupied_after_24_02_2022')?.area ?? 0;
   const liberated = areas.find((area) => area.type === 'liberated')?.area ?? 0;
   const unspecified = areas.find((area) => area.type === 'unspecified')?.area ?? 0;
-  return {
+  const occupiedInRussia = areas.find((area) => area.hash === '#01579b')?.area ?? 0;
+  const occupiedInRussiaUnspecified = areas.find((area) => area.hash === '#bdbdbd')?.area ?? 0;
+
+  const data: Areas = {
     liberated,
     occupied,
     unspecified,
   };
+
+  if (occupiedInRussia) {
+    data.occupiedInRussia = occupiedInRussia;
+  }
+  if (occupiedInRussiaUnspecified) {
+    data.occupiedInRussiaUnspecified = occupiedInRussiaUnspecified;
+  }
+  return data;
 }
 
 async function updateData() {
